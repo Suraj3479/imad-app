e71f6a38bd61dd6a5a10ae2ddb7eb7411b55f48a
Suraj3479 <36452376+Suraj3479@users.noreply.github.com>
2018-02-20 23:24:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,11 @@ var htmlTemplet=`
 return htmlTemplet;
 }
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/:articleName', function (req,res) {
   var articleName=req.params.articleName;
   res.send(createTemplet(articles[articleName]));
