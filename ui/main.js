55bd3da5fe56d278ab9509b6a868d7616baad5d7
Suraj3479 <36452376+Suraj3479@users.noreply.github.com>
2018-02-20 21:58:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+var element=document.getElementById('img');
+img.onclick= function(){
+    img.style.marginLeft='100px';
+}; 
