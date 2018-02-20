51cfff83dd55ce2e0b9de081546797e1fd12821d
Suraj3479 <36452376+Suraj3479@users.noreply.github.com>
2018-02-20 22:15:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft +1;
+    img.style.marginLeft=marginLeft+'px';
+    
+}
 img.onclick= function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRightt,50);
 }; 
