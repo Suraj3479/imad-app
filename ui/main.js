e0634d54d04892a1bfec46d88ba056f3328c68e4
Suraj3479 <36452376+Suraj3479@users.noreply.github.com>
2018-02-21 18:27:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,20 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='new value';
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft +1;
-    img.style.marginLeft=marginLeft+'px';
+var button=document.getElementById("counter");
+
+button.onclick=function(){
     
-}
-img.onclick= function(){
-    var interval=setInterval(moveRight,50);
-}; 
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readystate=== XMLHttpRequest.DONE){
+            
+            if(reque.ststatus===200){
+                var counter=request.respnseText;
+                var span=document.getElementById("count");
+               span.innerHTML=counter.toString();
+            }
+        }
+        
+    };
+request.open('Get','kishorsuraj1997.imad.hasura-app.io/counter',true);
+request.send(null);
+};
