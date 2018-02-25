b26beaeab199ca98bad35432efa7b99697eac8af
Suraj3479 <36452376+Suraj3479@users.noreply.github.com>
2018-02-25 13:38:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,6 @@ button.onclick=function(){
         }
         
     };
-request.open('GET','kishorsuraj1997.imad.hasura-app.io/counter',true);
+request.open('GET','kishorsuraj1997.imad.hasura-app.io',true);
 request.send(null);
 };
