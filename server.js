3d8a3cb3a180986be16d5d9113f5fc46a3f053dc
Suraj3479 <36452376+Suraj3479@users.noreply.github.com>
2018-03-25 03:09:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var pool = require('pg').pool;
+
+var config = {
+    user : 'kishorsuraj1997',
+    database : 'kishorsuraj1997',
+    host : 'db-imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB-PASSWORD
+};
+
 var app = express();
 app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 
 var articles={
    'article-one':{
@@ -63,6 +69,21 @@ var htmlTemplet=`
 `;
 return htmlTemplet;
 }
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT*FROM test',function(err,result){
+        if(err){
+          res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringfy(result));
+        }
+    });
+});
+
 
 var counter=0;
 app.get('/counter',function(req,res){
