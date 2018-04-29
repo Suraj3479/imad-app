fb6c2e38124123b458661f8b9eae28f7219c057d
Suraj3479 <36452376+Suraj3479@users.noreply.github.com>
2018-04-30 00:32:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', '20180122_125212.jpg'));
 });
 
 
